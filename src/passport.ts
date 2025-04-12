24964a65a637e6910f0970b114a296f7f43f8dc8
Samarth Bhutani <149926265+samarthhhbhutani@users.noreply.github.com>
2025-04-13 02:23:19 +0530

    Update passport.ts

--- src/passport.ts
+++ src/passport.ts
@@ -12,7 +12,7 @@ passport.use(
     {
       clientID: process.env.CLIENT_ID ||" ",
       clientSecret: process.env.CLIENT_SECRET ||" ",
-      callbackURL: "https://brainly-xi.vercel.app//auth/google/callback",
+      callbackURL: "https://brainlybackend-7fcv.onrender.com/auth/google/callback",
     },
     async (accessToken, refreshToken, profile, done) => {
       const user = {
